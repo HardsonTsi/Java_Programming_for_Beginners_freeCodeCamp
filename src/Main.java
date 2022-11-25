74b4be72555fa5b00b652e0a154f01b7653a8efb
TESSI Hardson <hardsontessi2@gmail.com>
2022-11-25 19:15:00 +0100

    Scanner

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,14 @@ public class Main {
         System.out.print("What is your name ?: ");
         String name = scanner.nextLine();
 
-        System.out.printf("Hello %s how are you ?", name);
+        System.out.printf("Hello %s, how old are you?: ", name);
+        int age = Integer.parseInt(scanner.nextLine());
+
+
+        System.out.printf("%d is an excellent age to start programming. What language do you prefer?: ", age);
+        String language = scanner.nextLine();
+
+        System.out.printf("%s is a very popular programming language.", language);
 
         scanner.close();
 
