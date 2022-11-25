3677f4bc134aeee8baa9de6d6a7a13dad55d25ff
TESSI Hardson <hardsontessi2@gmail.com>
2022-11-25 19:49:33 +0100

    Calculatrice

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,43 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("What is your name ?: ");
-        String name = scanner.nextLine();
+        System.out.print("Enter the first number: ");
+        double double1 = scanner.nextDouble();
 
-        System.out.printf("Hello %s, how old are you?: ", name);
-        int age = Integer.parseInt(scanner.nextLine());
+        System.out.print("Enter the secund number: ");
+        double double2 = scanner.nextDouble();
 
+        System.out.printf("first number = %f\n", double1);
+        System.out.printf("secund number = %f\n", double2);
 
-        System.out.printf("%d is an excellent age to start programming. What language do you prefer?: ", age);
-        String language = scanner.nextLine();
+        //clean
+        scanner.nextLine();
 
-        System.out.printf("%s is a very popular programming language.", language);
+        System.out.print("What operation do you want to perform ?: ");
+        String operation = scanner.nextLine();
+
+        switch (operation) {
+            case "sum":
+                System.out.printf("%f + %f = %f", double1, double2, double1 + double2);
+                break;
+            case "sub":
+                System.out.printf("%f - %f = %f", double1, double2, double1 - double2);
+                break;
+            case "mul":
+                System.out.printf("%f * %f = %f", double1, double2, double1 * double2);
+                break;
+            case "div":
+                if (double2 == 0) {
+                    System.out.println("Can not divide by zero.");
+                } else {
+                    System.out.printf("%f / %f = %f", double1, double2, double1 / double2);
+
+                }
+                break;
+            default:
+                System.out.printf("%s is not a supported operation");
+        }
+
 
         scanner.close();
 
