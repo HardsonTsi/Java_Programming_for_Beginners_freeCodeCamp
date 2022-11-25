fbd836b6806c07df508f51670f3a5d70464ef9d1
TESSI Hardson <hardsontessi2@gmail.com>
2022-11-25 20:20:06 +0100

    Comparaison de tableau

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,15 @@ public class Main {
 
         int numbers[] = {1, 2, 3, 4, 5};
 
-
         int copyOfNumbers[] = Arrays.copyOf(numbers, numbers.length);
-
-        int startingIndex = 1;
-        int endingIndex = 4;
-
-        copyOfNumbers = Arrays.copyOfRange(numbers, startingIndex, endingIndex);
 
         //Deprecated
         //int copyOfNumbers[] = numbers;
 
-        Arrays.fill(numbers, 0);
+        System.out.println(Arrays.equals(numbers, copyOfNumbers));
 
-        System.out.println(Arrays.toString(numbers));
-        System.out.println(Arrays.toString(copyOfNumbers));
+        //Deprecated
+        System.out.println(numbers == copyOfNumbers);
 
     }
 
