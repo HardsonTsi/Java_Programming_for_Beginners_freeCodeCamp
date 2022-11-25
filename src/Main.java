a45538f885d597805f0eff1c4ccb0e573bd8ca88
TESSI Hardson <hardsontessi2@gmail.com>
2022-11-25 18:15:04 +0100

    Incrémentation et décrémentation

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int number1 = 12;
-        int number2 = 15;
+        int score = 0;
+        int turns = 10;
 
-        //is equal to
-        System.out.println(number1 == number2);
+        score++;
+        turns--;
 
-        /*is not equal to*/
-        System.out.println(number1 != number2);
-
-        /*is greater than*/
-        System.out.println(number1 > number2);
-
-        /*is less than*/
-        System.out.println(number1 < number2);
-
-        /*is greater than or equal to*/
-        System.out.println(number1 >= number2);
-
-        /*is less than or equal to*/
-        System.out.println(number1 <= number2);
+        System.out.println(score);
+        System.out.println(turns);
 
 
     }
