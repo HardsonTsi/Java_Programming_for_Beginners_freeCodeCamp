e9b8d676c8adbcc62dd901dfd62138a8b7f96ceb
TESSI Hardson <hardsontessi2@gmail.com>
2022-11-25 20:04:28 +0100

    Trie de tableau

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-        char vowels[] = {'u', 'o', 'i', 'e', 'a'};
+        char[] vowels = {'u', 'o', 'i', 'e', 'a'};
 
-
-        //sort vowels
         Arrays.sort(vowels);
-        System.out.println(Arrays.toString(vowels));
 
         int startingIndex = 1;
         int endingIndex = 4;
-        Arrays.sort(vowels, startingIndex, endingIndex);
+
+        char key = 'e';
+        int foundItemIndex = Arrays.binarySearch(vowels, startingIndex, endingIndex, key);
 
         System.out.println(Arrays.toString(vowels));
-        System.out.println(vowels.length);
+        System.out.println(foundItemIndex);
+
+
     }
 
 }
