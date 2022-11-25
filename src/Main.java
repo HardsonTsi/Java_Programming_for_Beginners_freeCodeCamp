5f671aea7dd6b9aae6b1e7893ece694df405b332
TESSI Hardson <hardsontessi2@gmail.com>
2022-11-25 19:59:37 +0100

    Trie de tableau

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,22 @@
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter the first number: ");
-        double double1 = scanner.nextDouble();
-
-        System.out.print("Enter the secund number: ");
-        double double2 = scanner.nextDouble();
-
-        System.out.printf("first number = %f\n", double1);
-        System.out.printf("secund number = %f\n", double2);
-
-        //clean
-        scanner.nextLine();
-
-        System.out.print("What operation do you want to perform ?: ");
-        String operation = scanner.nextLine();
-
-        switch (operation) {
-            case "sum":
-                System.out.printf("%f + %f = %f", double1, double2, double1 + double2);
-                break;
-            case "sub":
-                System.out.printf("%f - %f = %f", double1, double2, double1 - double2);
-                break;
-            case "mul":
-                System.out.printf("%f * %f = %f", double1, double2, double1 * double2);
-                break;
-            case "div":
-                if (double2 == 0) {
-                    System.out.println("Can not divide by zero.");
-                } else {
-                    System.out.printf("%f / %f = %f", double1, double2, double1 / double2);
-
-                }
-                break;
-            default:
-                System.out.printf("%s is not a supported operation");
-        }
+        char vowels[] = {'u', 'o', 'i', 'e', 'a'};
 
 
-        scanner.close();
+        //sort vowels
+        Arrays.sort(vowels);
+        System.out.println(Arrays.toString(vowels));
 
+        int startingIndex = 1;
+        int endingIndex = 4;
+        Arrays.sort(vowels, startingIndex, endingIndex);
+
+        System.out.println(Arrays.toString(vowels));
+        System.out.println(vowels.length);
     }
 
 }
