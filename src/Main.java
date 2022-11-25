60cc7448ca0aae1fcde2fa9d27aa723180219021
TESSI Hardson <hardsontessi2@gmail.com>
2022-11-25 21:25:19 +0100

    none

--- src/Main.java
+++ src/Main.java
@@ -12,22 +12,9 @@ public class Main {
         examScores.put("Bengali", 100);
         examScores.put("Computer programming", 100);
 
-        examScores.putIfAbsent("Math", 70);
-
-        examScores.replace("Math", 70);
-
-        System.out.println(examScores.getOrDefault("Religion", 0));
-
-        System.out.println(examScores);
-
-        System.out.println(examScores.containsKey("Sociology"));
-        examScores.remove("Sociology");
-
-        System.out.println(examScores.containsValue(100));
-
-        examScores.clear();
-
-        System.out.println(examScores.size());
+        examScores.forEach((key, value) -> {
+            System.out.printf("%s --> %d\n", key, value);
+        });
 
     }
 }
