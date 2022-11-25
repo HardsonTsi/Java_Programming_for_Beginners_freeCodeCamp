fa43e6af4dd47b48f0332b0a64b11b043b0fe078
TESSI Hardson <hardsontessi2@gmail.com>
2022-11-25 18:17:55 +0100

    Préincrémentation et Postincrémentation

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int score = 0;
-        int turns = 10;
+        int number = 55;
 
-        score++;
-        turns--;
-
-        System.out.println(score);
-        System.out.println(turns);
-
+        System.out.println(number++);
+        System.out.println(++number);
 
     }
 
